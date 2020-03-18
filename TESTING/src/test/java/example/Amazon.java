3a331763c5cc212081f2ae38159655d1193dc394
hello9263 <Steve.fox9263@gmail.com>
2020-03-18 04:53:47 -0700

    new testcase from qwerty

---
+++ TESTING/src/test/java/example/Amazon.java
@@ -0,0 +1,41 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class Amazon {
+	 private WebDriver driver;	
+  @Test
+  public void f() {
+	  driver.get("https://www.amazon.com/");
+	  driver.get("https://www.amazon.com/");
+	    driver.manage().window().setSize(new Dimension(1552, 840));
+	    driver.findElement(By.id("twotabsearchtextbox")).click();
+	    driver.findElement(By.id("twotabsearchtextbox")).sendKeys("S10");
+	    driver.findElement(By.id("twotabsearchtextbox")).sendKeys(Keys.ENTER);
+	    driver.findElement(By.cssSelector(".a-color-state")).click();
+	    String text=driver.findElement(By.cssSelector(".a-color-state")).getText();
+	    String actualtext="\"S10\"";
+	    Assert.assertEquals(text,actualtext);
+	    driver.close();
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\DELL\\chromedriver.exe");
+      
+	    driver = new ChromeDriver();  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
