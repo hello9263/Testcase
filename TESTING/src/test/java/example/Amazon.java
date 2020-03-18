192ed7e3d2999ee875c85ab89e806becbda2a2f3
hello9263 <Steve.fox9263@gmail.com>
2020-03-18 10:02:07 -0700

    co

--- TESTING/src/test/java/example/Amazon.java
+++ TESTING/src/test/java/example/Amazon.java
@@ -18,11 +18,11 @@ public class Amazon {
 	driver.get("https://www.amazon.com/");
 	    driver.manage().window().setSize(new Dimension(1552, 840));
 	    driver.findElement(By.id("twotabsearchtextbox")).click();
-	    driver.findElement(By.id("twotabsearchtextbox")).sendKeys("\"S10\"");
+	    driver.findElement(By.id("twotabsearchtextbox")).sendKeys("S10");
 	    driver.findElement(By.id("twotabsearchtextbox")).sendKeys(Keys.ENTER);
 	    driver.findElement(By.cssSelector(".a-color-state")).click();
 	    String text=driver.findElement(By.cssSelector(".a-color-state")).getText();
-	    String actualtext="S10";
+	    String actualtext="\"S10\"";
 	    Assert.assertEquals(text, actualtext);
 	   
   }
